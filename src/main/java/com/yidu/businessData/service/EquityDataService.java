8191ac1471f6f0749e9c8c497b9dc2de927b46c0
wzh <1695573004@qq.com>
2020-09-04 12:01:42 +0800

    魏志豪 2020-9-4 中午

--- src/main/java/com/yidu/businessData/service/EquityDataService.java
+++ src/main/java/com/yidu/businessData/service/EquityDataService.java
@@ -3,11 +3,16 @@ package com.yidu.businessData.service;
 import com.yidu.businessData.pojo.EquityData;
 
 import java.util.List;
+import java.util.Map;
+
 /**
  *@author wzh
  *date 2020-9-2
  * 权益数据设置服务器Service
  */
 public interface EquityDataService {
-    public List<EquityData> selectEquityDataMapper();
+    public int insertEquityData(EquityData equityData);
+    public int  deleteEquityData(String  equityId);
+    public int updateEquityData(EquityData equityData);
+    public Map<String,Object> selectEquityData(String pageSize, String page);
 }
