31643a5de2737161e308d1bc3f31fef5167791c8
hahaha <1145430581@qq.com>
2020-09-07 19:30:17 +0800

    wufeiyun 2020-9-7

--- src/main/java/com/yidu/inventoryManage/pojo/SecuritiesInventory.java
+++ src/main/java/com/yidu/inventoryManage/pojo/SecuritiesInventory.java
@@ -1,9 +1,13 @@
 package com.yidu.inventoryManage.pojo;
 
+import com.yidu.businessParameter.pojo.AccountPojo;
+import com.yidu.businessParameter.pojo.Fund;
+import com.yidu.businessParameter.pojo.SecuritiesPojo;
+
 /**
  * @type 证券库存
  * @author wufeiyun
- * @date 2020-9-1 10:08
+ * @date 2020-9-7 10:08
  *@证券库存实体类
  */
 
@@ -12,11 +16,15 @@ public class SecuritiesInventory {
     private String dateTime;  //证券日期
     private String securitiesId; //证券信息表ID
     private String fundId;  //基金表ID
+    private String accountId; //    账户id
     private int securityPeriodFlag; //是否从其他系统导入的期初数据
     private int securitiesNum;  //证券的数量
     private double price;
     private double total;  //总金额
     private String securitiesInventoryDesc;   //备注
+    private AccountPojo accountPojo;
+    private Fund fund;
+    private SecuritiesPojo securitiesPojo;
     public SecuritiesInventory() {
     }
 
@@ -90,6 +98,38 @@ public class SecuritiesInventory {
 
     public void setSecuritiesInventoryDesc(String securitiesInventoryDesc) {
         this.securitiesInventoryDesc = securitiesInventoryDesc;
+    }
+
+    public SecuritiesPojo getSecuritiesPojo() {
+        return securitiesPojo;
+    }
+
+    public void setSecuritiesPojo(SecuritiesPojo securitiesPojo) {
+        this.securitiesPojo = securitiesPojo;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public AccountPojo getAccountPojo() {
+        return accountPojo;
+    }
+
+    public void setAccountPojo(AccountPojo accountPojo) {
+        this.accountPojo = accountPojo;
+    }
+
+    public Fund getFund() {
+        return fund;
+    }
+
+    public void setFund(Fund fund) {
+        this.fund = fund;
     }
 
     public SecuritiesInventory(String securitiesInventoryId, String dateTime, String securitiesId, String fundId, int securityPeriodFlag, int securitiesNum, double price, double total, String securitiesInventoryDesc) {
---
+++ src/main/java/com/yidu/inventoryManage/service/impl/SecuritiesInventoryServiceImpl.java
@@ -0,0 +1,61 @@
+package com.yidu.inventoryManage.service.impl;
+
+import com.yidu.inventoryManage.mapper.SecuritiesInventoryMapper;
+import com.yidu.inventoryManage.pojo.SecuritiesInventory;
+import com.yidu.inventoryManage.service.SecuritiesInventoryService;
+import com.yidu.util.DbUtil;
+import com.yidu.util.SysTableNameListUtil;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+/*
+  @type:证券库存实现类
+ *@author wufeiyun
+ * time 2020-9-7 15:36
+  version 1.0
+ * */
+@Service
+public class SecuritiesInventoryServiceImpl implements SecuritiesInventoryService {
+    @Resource
+    SecuritiesInventoryMapper securitiesInventoryMapper;
+    @Resource
+    DbUtil dbUtil;
+
+    @Override
+    public HashMap selectSecuritiesInventory(int page,int limit,String sreachTime,String sreachId) {
+        HashMap securitiesInventoryMap = new HashMap();
+        securitiesInventoryMap.put("p_tableName","(select * from securitiesInventory se  join SECURITIES s on se.SECURITIESID=s.SECURITIESID)");
+        if(sreachTime!=null&&sreachTime!=""){
+            securitiesInventoryMap.put("p_condition"," and dateTime like '%"+sreachTime+"%'");
+        }
+        else if (sreachId!=null&&sreachId!=""){
+            securitiesInventoryMap.put("p_condition"," and securitiesInventoryId like '%" +sreachId+"%' and dateTime like '%"+ sreachTime+"%'");
+        }else {
+            securitiesInventoryMap.put("p_condition","");
+        }
+        securitiesInventoryMap.put("p_pageSize",limit);
+        securitiesInventoryMap.put("p_page",page);
+        securitiesInventoryMap.put("p_count",0);
+        securitiesInventoryMap.put("p_cursor",null);
+        securitiesInventoryMapper.selectSecuritiesInventory(securitiesInventoryMap);
+        return securitiesInventoryMap;
+    }
+
+    @Override
+    public int updateSecuritiesInventory(SecuritiesInventory securitiesInventory) {
+        int i = securitiesInventoryMapper.updateSecuritiesInventory(securitiesInventory);
+        return i;
+    }
+
+    @Override
+    public int deleteSecuritiesInventory(int securitiesInventoryId) {
+        int i = securitiesInventoryMapper.deleteSecuritiesInventory(securitiesInventoryId);
+        return i;
+    }
+    public int insertSecuritiesInventory(SecuritiesInventory securitiesInventory){
+        securitiesInventory.setSecuritiesInventoryId(dbUtil.requestDbTableMaxId(SysTableNameListUtil.M));
+        int i = securitiesInventoryMapper.insertSecuritiesInventory(securitiesInventory);
+        return i;
+    }
+}
