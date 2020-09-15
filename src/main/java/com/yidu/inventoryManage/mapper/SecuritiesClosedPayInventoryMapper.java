e0ea6740c706dd9730b3ed8f8d0999fe38d9ba1c
zoji <2280748803@qq.com>
2020-09-15 16:35:03 +0800

    name:邹稷 title：第一次修改 date：2020/9/15

--- src/main/java/com/yidu/inventoryManage/mapper/SecuritiesClosedPayInventoryMapper.java
+++ src/main/java/com/yidu/inventoryManage/mapper/SecuritiesClosedPayInventoryMapper.java
@@ -1,6 +1,9 @@
 package com.yidu.inventoryManage.mapper;
 
+import com.yidu.inventoryManage.pojo.SeInventoryPayEntity;
 import com.yidu.inventoryManage.pojo.SecuritiesClosedPayInventoryPojo;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.HashMap;
@@ -19,4 +22,21 @@ public interface SecuritiesClosedPayInventoryMapper {
     public int insertSecuritiesClosedPayInventory(SecuritiesClosedPayInventoryPojo securitiesClosedPayInventoryPojo);
     public int updateSecuritiesClosedPayInventory(SecuritiesClosedPayInventoryPojo securitiesClosedPayInventoryPojo);
     public int deleteSecuritiesClosedPayInventory(List securitiesClosedPayInventoryIdList);
+
+    /**
+     * 证券应收应付的实体类
+     * @param securitiesClosedPayInventoryPojo
+     */
+    @Insert("insert into securitiesClosedPayInventory values (#{securitiesClosedPayInventoryId}," +
+            "#{dateTime},#{fundId},#{securitiesId},#{securitiesType},#{flag},#{totalPrice},#{securitiesClosedPayDesc},#{securityPeriodFlag})")
+    public void insertTaInventoryPayMapper(SecuritiesClosedPayInventoryPojo securitiesClosedPayInventoryPojo);
+
+
+    /**
+     * 根据日期删除的方法
+     * @param date
+     */
+    @Delete("delete securitiesClosedPayInventory where dateTime=#{date}")
+    public void delectDateTaInventoryPayMapper(String date);
+
 }
