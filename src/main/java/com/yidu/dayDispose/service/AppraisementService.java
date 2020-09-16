a4008dd1b8686937fdb1558a2e07b075fab1be45
hahaha <1145430581@qq.com>
2020-09-16 14:21:37 +0800

    wufeiyun 2020-09-16

--- src/main/java/com/yidu/dayDispose/service/AppraisementService.java
+++ src/main/java/com/yidu/dayDispose/service/AppraisementService.java
@@ -2,20 +2,23 @@ package com.yidu.dayDispose.service;
 
 import com.yidu.businessData.pojo.TransactionData;
 import com.yidu.dayDispose.pojo.Appraisement;
+import com.yidu.dayDispose.pojo.ValuationProcessing;
 import com.yidu.inventoryManage.pojo.CashClosedPayInventory;
-import com.yidu.inventoryManage.pojo.SecuritiesClosedPayInventory;
 import com.yidu.inventoryManage.pojo.SecuritiesClosedPayInventoryPojo;
 
 import java.util.HashMap;
 import java.util.List;
 
 public interface AppraisementService {
+    //查询表格内容
+    public List<ValuationProcessing> selectBiaoge();
+
     //查询应收应付状态
     public List<Appraisement> selectValuationProcessing();
     //证券库存join行情表查询
     public HashMap selectStockarket();
     //删除证券应收应付库存
-    public int deleteSecuritiesClosedPayInventory(SecuritiesClosedPayInventory securitiesClosedPayInventory);
+    public int deleteSecuritiesClosedPayInventory(SecuritiesClosedPayInventoryPojo securitiesClosedPayInventoryPojo);
 
     //查交易数据 按证券代码分组 插入证券应收应付库存
     public HashMap selectTransactionData();
