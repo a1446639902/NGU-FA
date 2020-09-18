18a88f76c3aa52f4891ecd2e34e2b7d1e92b9aa2
zoji <2280748803@qq.com>
2020-09-18 20:14:18 +0800

    name:邹稷 title：实现了现金头寸表功能 date：2020/9/18

--- src/main/java/com/yidu/reportManage/mapper/AvailableCashMapper.java
+++ src/main/java/com/yidu/reportManage/mapper/AvailableCashMapper.java
@@ -1,12 +1,18 @@
 package com.yidu.reportManage.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Map;
+
 /**
  * 报表管理：现金头寸报表的Mapper层
  * date：2020/9/18
  * @Mr.Zou
  */
+@Mapper
 public interface AvailableCashMapper {
 
-
+    void selectAvailable(Map map);
 
 }
--- src/main/java/com/yidu/reportManage/pojo/AvailableCashEntity.java
+++ src/main/java/com/yidu/reportManage/pojo/AvailableCashEntity.java
@@ -6,4 +6,41 @@ package com.yidu.reportManage.pojo;
  * @Mr.Zou
  **/
 public class AvailableCashEntity {
+
+    private String accountName;
+    private double cashBlance;
+
+    public AvailableCashEntity() {
+    }
+
+    public AvailableCashEntity(String accountName, double cashBlance) {
+        this.accountName = accountName;
+        this.cashBlance = cashBlance;
+    }
+
+    @Override
+    public String toString() {
+        return "AvailableCashPositionTable{" +
+                "accountName='" + accountName + '\'' +
+                ", cashBlance=" + cashBlance +
+                '}';
+    }
+
+    public double getCashBlance() {
+        return cashBlance;
+    }
+
+    public void setCashBlance(double cashBlance) {
+        this.cashBlance = cashBlance;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+
 }
