9fec2f8699a189ea1a760b2dc83547cf98b32bf2
wzh <1695573004@qq.com>
2020-09-21 11:11:11 +0800

    魏志豪 2020-9-21

--- src/main/java/com/yidu/businessDispose/service/EquityDisposeService.java
+++ src/main/java/com/yidu/businessDispose/service/EquityDisposeService.java
@@ -6,7 +6,29 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 @Service
 public interface EquityDisposeService {
+    /**
+     * 权益处理查询方法
+     * @param pageSize
+     * @param page
+     * @param equitiesType
+     * @param equitiesExright
+     * @param disposeStatus
+     * @return
+     */
     public Map<String,Object> selectEquityDispose(String pageSize, String page, String equitiesType, String equitiesExright,String disposeStatus);
+
+    /**
+     * 权益处理修改方法
+     * @param equityDisPose
+     * @param request
+     * @return
+     */
     public int updateEquityDispose(String equityDisPose,HttpServletRequest request);
+
+    /**
+     * 权益处理修改方法
+     * @param equityDataId
+     * @return
+     */
     public int updateEquityDisposeTwo(String equityDataId);
 }
