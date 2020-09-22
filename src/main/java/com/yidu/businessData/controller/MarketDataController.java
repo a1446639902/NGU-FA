7cf89a18d73789225dbb46cbe8c4d0768e0a108b
hahaha <1145430581@qq.com>
2020-09-22 16:29:04 +0800

    wufeiyun 2020-09-22下午

--- src/main/java/com/yidu/businessData/controller/MarketDataController.java
+++ src/main/java/com/yidu/businessData/controller/MarketDataController.java
@@ -32,10 +32,12 @@ public class MarketDataController {
     @Resource
     MarketDataService marketDataService;
 
+    //调用工具类
     @Resource
     DbUtil dbUtil;
     @NGULog(message = "删除行情数据")
    @RequestMapping("/deleteMarKetDate")
+    //前端传marketIds字符串 进行切割循环调用方法
     public int deleteMarKetDate(String marketIds){
         int i=0;
        System.out.println(marketIds);
@@ -49,6 +51,7 @@ public class MarketDataController {
    }
     @NGULog(message = "修改行情数据")
     @RequestMapping("/updateMarKetDate")
+    //前端传入MarketData类型的实体类参数
     public int updateMarKetDate(MarketData marketData){
         System.out.println(marketData);
         int i = marketDataService.updateMarketDate(marketData);
@@ -60,19 +63,24 @@ public class MarketDataController {
     public int insertMarKetDate(MarketData marketData){
         System.out.println("增加进来了===========================");
         System.out.println(marketData);
+        //从工具类获取最大Id
         marketData.setMarketId(dbUtil.requestDbTableMaxId(SysTableNameListUtil.M));
         System.out.println(marketData);
         return marketDataService.insertMarketDate(marketData);
     }
     @NGULog(message = "查询行情数据")
     @RequestMapping("/selectMarKetDate")
+    //page 页码   limit 每页的条数  searchMarketId和searchTime是查询条件
     public HashMap selectMarKetDate(int page,int limit,String searchMarketId,String searchTime){
         System.out.println("进来了====================================================");
         System.out.println(searchMarketId+","+searchTime);
         HashMap hashMap = marketDataService.selectMarKetDate1(page, limit,searchMarketId,searchTime);
+        //通过(int)hashMap.get("p_count")获得count条数
         int count = (int)hashMap.get("p_count");
+        //通过(List<MarketData>)hashMap.get("p_cursor")获得MarketData 类型的集合
         List<MarketData> marketDataList = (List<MarketData>)hashMap.get("p_cursor");
         System.out.println(marketDataList);
+        //返回到页面的集合
         HashMap stockofSecuritiesMap = new HashMap();
         stockofSecuritiesMap.put("code",0);
         stockofSecuritiesMap.put("count",count);
@@ -87,8 +95,10 @@ public class MarketDataController {
     public Map<String, Object> uploadMarket(MultipartFile file) throws IOException {
         Map<String,Object> map = new HashMap<>();
         List<MarketData> list1 = marketDateUtil.getList(MarketData.class,file.getInputStream(),0);
+        System.out.println(list1.size());
         for (MarketData marketData : list1) {
             System.out.println(marketData);
+
             int i = marketDataService.insertMarketDate(marketData);
             System.out.println(i);
         }
--- src/main/java/com/yidu/dayDispose/controller/RevenueProvisionController.java
+++ src/main/java/com/yidu/dayDispose/controller/RevenueProvisionController.java
@@ -34,14 +34,17 @@ public class RevenueProvisionController {
     SecuritiesClosedPayService securitiesClosedPayService;
     @Resource
     DbUtil dbUtil;
-     static double custodyMoney;
     @NGULog(message = "查询现金账户join现金库存")
     @RequestMapping("selectRevenueProvision")
+   // int page, int limit,String statDate  page 页码 limit 每页条数  statDate  前端传的日期 查询条件
     public HashMap selectRevenueProvision(int page, int limit,String statDate){
         HashMap hashMap = revenueProvisionService.selectRevenueProvision(page, limit,statDate);
+        // 通过(int)hashMap.get("p_count")获得count条数
         int count = (int)hashMap.get("p_count");
+        //通过(List<RevenueProvision>)hashMap.get("p_cursor") 获得RevenueProvision类型的集合
         List<RevenueProvision> revenueProvisionList = (List<RevenueProvision>)hashMap.get("p_cursor");
         System.out.println(revenueProvisionList);
+        //返回集合数据到前端
         HashMap revenueProvisionMap = new HashMap();
         revenueProvisionMap.put("code",0);
         revenueProvisionMap.put("count",count);
@@ -55,9 +58,12 @@ public class RevenueProvisionController {
     public HashMap selectBondInterest(int page ,int limit,String statDate){
         System.out.println("第二个"+statDate);
         HashMap hashMap = revenueProvisionService.selectBondInterest(page, limit ,statDate);
+        // 通过(int)hashMap.get("p_count")获得count条数
         int count = (int)hashMap.get("p_count");
+        //通过(List<BondInterest>)hashMap.get("p_cursor");获得BondInterest类型的集合
         List<BondInterest> bondInterestList = (List<BondInterest>)hashMap.get("p_cursor");
         System.out.println(bondInterestList);
+        //返回bondInterestMap 集合到前端
         HashMap bondInterestMap = new HashMap();
         bondInterestMap.put("code",0);
         bondInterestMap.put("count",count);
@@ -71,7 +77,9 @@ public class RevenueProvisionController {
 
         HashMap hashMap = revenueProvisionService.selectTwoFees(page, limit, statDate);
         System.out.println(statDate+"=============================");
+        // 通过(int)hashMap.get("p_count")获得count条数
         int count = (int)hashMap.get("p_count");
+        //通过(List<TwoFees>)hashMap.get("p_cursor"); 获得TwoFees类型的集合
         List<TwoFees> twoFeesList = (List<TwoFees>)hashMap.get("p_cursor");
         System.out.println(twoFeesList);
         HashMap twoFeesMap = new HashMap();
@@ -84,27 +92,34 @@ public class RevenueProvisionController {
 
     @NGULog(message = "现金信息页面选中信息")
     @RequestMapping("CountingCash")
+    //String cash,HttpServletRequest request cash是前端返回的json对象字符串
     public int CountingCash(String cash,HttpServletRequest request){
         int i=0;
         System.out.println("进来了===============================================");
         System.out.println(cash);
+        //通过调用sonUtil.jsonToArrayList(cash, RevenueProvision.class);工具类得到RevenueProvision类型的集合
         List<RevenueProvision> revenueProvisionList = JsonUtil.jsonToArrayList(cash, RevenueProvision.class);
         CashClosedPayPojo cashClosedPayPojo = new CashClosedPayPojo();
+        //遍历集合
         for (RevenueProvision revenueProvision : revenueProvisionList) {
+            //setDateTime 放时间
             cashClosedPayPojo.setDateTime(revenueProvision.getDateTime());
+            //setFundId  放基金Id
             cashClosedPayPojo.setFundId(revenueProvision.getFundId());
+            //setAccountId 账户Id
             cashClosedPayPojo.setAccountId(revenueProvision.getAccountId());
-            System.out.println(cashClosedPayPojo+"===============================================");
-
-
+            //dbUtil.requestDbTableMaxId(SysTableNameListUtil.CCP) 通过工具类获得最大Id
                 String cashClosedPayId = dbUtil.requestDbTableMaxId(SysTableNameListUtil.CCP);
+
                 cashClosedPayPojo.setCashClosedPayId(cashClosedPayId);
-                cashClosedPayPojo.setFundId(revenueProvision.getFundId());
-                cashClosedPayPojo.setAccountId(revenueProvision.getAccountId());
+//                cashClosedPayPojo.setFundId(revenueProvision.getFundId());
+//                cashClosedPayPojo.setAccountId(revenueProvision.getAccountId());
+            //setServiceType 类型为3
                 cashClosedPayPojo.setServiceType(3);
+                //.setAmount 放入利息  getInterest get利息
                 cashClosedPayPojo.setAmount(revenueProvision.getInterest());
                 System.out.println(revenueProvision.getInterest());
-                cashClosedPayPojo.setDateTime(revenueProvision.getDateTime());
+//                cashClosedPayPojo.setDateTime(revenueProvision.getDateTime());
                 cashClosedPayPojo.setFlag(1);
                 i = cashClosedPayService.insertCashClosedPay(cashClosedPayPojo,request);
                 if(i>0){
@@ -115,32 +130,8 @@ public class RevenueProvisionController {
                     }
                 }
 
-
-
-
         }
-//        JSONArray jsonArray = JSONArray.fromObject(cash);
-//        System.out.println(jsonArray);
-//        List list = (List)JSONArray.toCollection(jsonArray, RevenueProvision.class);
-//        for (Object o : list) {
-//            Iterator it = list.iterator();
-//            while(it.hasNext()){
-//                RevenueProvision p = (RevenueProvision)it.next();
-//                System.out.println(p);
-//                cashClosedPayService.deleteNew(p);
-//                String cashClosedPayId = dbUtil.requestDbTableMaxId(SysTableNameListUtil.CCP);
-//                CashClosedPayPojo cashClosedPayPojo = new CashClosedPayPojo();
-//                cashClosedPayPojo.setCashClosedPayId(cashClosedPayId);
-//                cashClosedPayPojo.setFundId(p.getFundId());
-//                cashClosedPayPojo.setAccountId(p.getAccountId());
-//                cashClosedPayPojo.setServiceType(1);
-//                cashClosedPayPojo.setAmount(p.getCashBlance());
-//                cashClosedPayPojo.setDateTime(p.getDateTime());
-//                cashClosedPayPojo.setFlag(1);
-//                System.out.println(cashClosedPayPojo);
-//                i = cashClosedPayService.insertCashClosedPay(cashClosedPayPojo);
-//            }
-//        }
+
         return i;
     }
 
@@ -150,98 +141,94 @@ public class RevenueProvisionController {
         int i=0;
 
         System.out.println(Securities);
+        //从session获得accountId  GetAccountUtil.getAccountId(request);
         String accountId = GetAccountUtil.getAccountId(request);
         System.out.println("进来了==============================================="+accountId);
         List<BondInterest> bondInterestList = JsonUtil.jsonToArrayList(Securities, BondInterest.class);
         for (BondInterest bondInterest : bondInterestList) {
             System.out.println(bondInterest.getAccountId()+"这是getAccountId======================================");
+            //创建SecuritiesClosedPayPojo实体类
             SecuritiesClosedPayPojo securitiesClosedPay1 = new SecuritiesClosedPayPojo();
+            dbUtil.requestDbTableMaxId(SysTableNameListUtil.SCP);//获得最大ID
             String cashClosedPayId = dbUtil.requestDbTableMaxId(SysTableNameListUtil.SCP);
             System.out.println(cashClosedPayId);
+            //放入setSecuritiesClosedPayId(cashClosedPayId);
             securitiesClosedPay1.setSecuritiesClosedPayId(cashClosedPayId);
+            //setFundId(bondInterest.getFundId());
             securitiesClosedPay1.setFundId(bondInterest.getFundId());
+            //securitiesClosedPay1.setAccountId(accountId);
             securitiesClosedPay1.setAccountId(accountId);
+            //setServiceType(3);
             securitiesClosedPay1.setServiceType(3);
+            //setAmount(bondInterest.getInterest());
+            //Amount是现金应收应付表的利息  Interest是债券信息算出的利息
             securitiesClosedPay1.setAmount(bondInterest.getInterest());
+            //setDateTime
             securitiesClosedPay1.setDateTime(bondInterest.getDateTime());
+            //setFlag(1);
             securitiesClosedPay1.setFlag(1);
+            //证券Id
             securitiesClosedPay1.setSecuritiesId(bondInterest.getSecuritiesId());
             System.out.println(securitiesClosedPay1);
+            //先增
             i = securitiesClosedPayService.insertSecuritiesClosedPay(securitiesClosedPay1);
             System.out.println(i+"插入证券应收应付的I===========================");
-            SecuritiesClosedPayPojo securitiesClosedPay = new SecuritiesClosedPayPojo();
-            securitiesClosedPay.setDateTime(bondInterest.getDateTime());
-            securitiesClosedPay.setFundId(bondInterest.getFundId());
-            securitiesClosedPay.setSecuritiesId(bondInterest.getSecuritiesId());
-            System.out.println(securitiesClosedPay);
-            i = securitiesClosedPayService.deleteSecuritiesClosedPayByPojo(securitiesClosedPay);
-             if(i>0){
-                 System.out.println(i+"===================================这是一个删除证券应收应付的结果");
-                 String cashClosedPayId1 = dbUtil.requestDbTableMaxId(SysTableNameListUtil.SCP);
-                 System.out.println(cashClosedPayId1);
-                 securitiesClosedPay1.setSecuritiesClosedPayId(cashClosedPayId1);
-                 securitiesClosedPay1.setFundId(bondInterest.getFundId());
-                 securitiesClosedPay1.setAccountId(accountId);
-                 securitiesClosedPay1.setServiceType(3);
-                 securitiesClosedPay1.setAmount(bondInterest.getInterest());
-                 securitiesClosedPay1.setDateTime(bondInterest.getDateTime());
-                 securitiesClosedPay1.setFlag(1);
-                 securitiesClosedPay1.setSecuritiesId(bondInterest.getSecuritiesId());
-                 System.out.println(securitiesClosedPay1);
-                 i = securitiesClosedPayService.insertSecuritiesClosedPay(securitiesClosedPay1);
-                 System.out.println(i+"插入证券应收应付的I===========================");
-             }
+            if(i>0){
+                //删
+                i = securitiesClosedPayService.deleteSecuritiesClosedPayByPojo(securitiesClosedPay1);
+                if(i>0){
+                    //再增
+                    i = securitiesClosedPayService.insertSecuritiesClosedPay(securitiesClosedPay1);
+                }
+            }
 
         }
         return i;
     }
     @NGULog(message = "两费信息页面选中信息")
     @RequestMapping("StatisticalTwoFees")
+    //前端返回的json字符串名字TwoFees  String TwoFees, HttpServletRequest request
     public int statisticalTwoFees(String TwoFees, HttpServletRequest request){
         int i=0;
         System.out.println("进来了===============================================");
         System.out.println(TwoFees);
+        //通过JsonUtil.jsonToArrayList(TwoFees, TwoFees.class); 得到twoFeesList类型的集合
         List<TwoFees> twoFeesList = JsonUtil.jsonToArrayList(TwoFees, TwoFees.class);
         for (TwoFees twoFees: twoFeesList) {
             //如果是第一次查询现金应收应付没有数据，先增加
             System.out.println(twoFees+"===================================================");
             CashClosedPayPojo cashClosedPayPojo = new CashClosedPayPojo();
+            //dbUtil.requestDbTableMaxId(SysTableNameListUtil.CCP);获得最大ID
             String cashClosedPayId = dbUtil.requestDbTableMaxId(SysTableNameListUtil.CCP);
+            //setCashClosedPayId
             cashClosedPayPojo.setCashClosedPayId(cashClosedPayId);
+            //setFundId
             cashClosedPayPojo.setFundId(twoFees.getFundId());
+            //setAccountId
             cashClosedPayPojo.setAccountId(twoFees.getAccountId());
+            //setServiceType(1);
             cashClosedPayPojo.setServiceType(1);
+            //setAmount(twoFees.getManagementMoney());  数据库字段名不一样
             cashClosedPayPojo.setAmount(twoFees.getManagementMoney());
+            //setDateTime(twoFees.getValueStatisticsDate()); 数据库字段名不一样
             cashClosedPayPojo.setDateTime(twoFees.getValueStatisticsDate());
             cashClosedPayPojo.setFlag(1);
-            custodyMoney = twoFees.getCustodyMoney();
-            System.out.println(custodyMoney+"================================成员变量1");
+
             //增加管理费利息数据
-            cashClosedPayService.insertCashClosedPay(cashClosedPayPojo,request);
+            int i1=cashClosedPayService.insertCashClosedPay(cashClosedPayPojo,request);
             //增加托管费利息数据
             cashClosedPayPojo.setServiceType(2);
+            double custodyMoney = twoFees.getCustodyMoney();
             cashClosedPayPojo.setAmount(custodyMoney);
-            //按条件删除现金应收应付
-            System.out.println("两费========================="+cashClosedPayPojo);
-            i = cashClosedPayService.deleteNew(cashClosedPayPojo);
-            System.out.println(1+"删除两费的状态=========================");
-            if(i>0){
-                String cashClosedPayId1 = dbUtil.requestDbTableMaxId(SysTableNameListUtil.CCP);
-                cashClosedPayPojo.setCashClosedPayId(cashClosedPayId1);
-                cashClosedPayPojo.setFundId(twoFees.getFundId());
-                cashClosedPayPojo.setAccountId(twoFees.getAccountId());
-                cashClosedPayPojo.setServiceType(1);
-                cashClosedPayPojo.setAmount(twoFees.getManagementMoney());
-                cashClosedPayPojo.setDateTime(twoFees.getValueStatisticsDate());
-                cashClosedPayPojo.setFlag(-1);
-                i = cashClosedPayService.insertCashClosedPay(cashClosedPayPojo,request);
-                cashClosedPayPojo.setServiceType(2);
-                System.out.println(this.custodyMoney+"================================这是成员变量");
-                cashClosedPayPojo.setAmount(this.custodyMoney);
-                System.out.println(twoFees);
-                System.out.println(custodyMoney);
-                i = cashClosedPayService.insertCashClosedPay(cashClosedPayPojo,request);
-                System.out.println(i+"这是两费的i====================================");
+            int i2 = cashClosedPayService.insertCashClosedPay(cashClosedPayPojo,request);
+            if(i1>0&&i2>0){
+                i = cashClosedPayService.deleteNew(cashClosedPayPojo);
+                if(i>0){
+                    i = cashClosedPayService.insertCashClosedPay(cashClosedPayPojo,request);
+                    cashClosedPayPojo.setServiceType(1);
+                    cashClosedPayPojo.setAmount(twoFees.getManagementMoney());
+                    i=cashClosedPayService.insertCashClosedPay(cashClosedPayPojo,request);
+                }
             }
 
         }
